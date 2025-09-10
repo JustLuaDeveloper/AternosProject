f54b8f7da4aa80217b170a03d2408d56abeca116
JustLuaDeveloper <stasmaks5423@gmail.com>
2025-09-10 20:11:53 +0300

    Create Init.lua

---
+++ AternosProtect/utils/Init.lua
@@ -0,0 +1,23 @@
+local function req(url)
+    return loadstring(game:HttpGet(url))()
+end
+local items = {
+    AntiHttp = "https://raw.githubusercontent.com/JustLuaDeveloper/AternosProject/main/AternosProtect/utils/Antihttp.lua",
+    SendWebHook = "https://raw.githubusercontent.com/Player01010111Cheater/AternosClient/main/util/items/SendWebHook.lua",
+}
+
+local utils = {}
+
+for name, item in pairs(items) do
+    if type(item) == "string" and item:match("^https?://") then
+        utils[name] = function(...)
+            local f = req(item)
+            utils[name] = f
+            return f(...)
+        end
+    else
+        utils[name] = item
+    end
+end
+
+return utils
