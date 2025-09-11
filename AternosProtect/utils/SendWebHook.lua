24f72195ed570e82262fb08b134885a5dcda4abb
JustLuaDeveloper <stasmaks5423@gmail.com>
2025-09-11 11:32:53 +0300

    Update SendWebHook.lua

--- AternosProtect/utils/SendWebHook.lua
+++ AternosProtect/utils/SendWebHook.lua
@@ -1,1 +1,16 @@
-
+local https = game:GetService("HttpService")
+return function(channel_id,webhook ,text)
+    local data = {
+        ["content"] = text,
+        ["username"] = "AternosProtect"
+    }
+    local body = https:JSONEncode(data)
+    request({
+        Url = "https://discord.com/api/webhooks/" .. channel_id .."/" .. webhook,
+        Method = "POST",
+        Headers = {
+            ["Content-Type"] = "application/json"
+        },
+        Body = body
+    })
+end
